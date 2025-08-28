6f4eff20f5a07a1e92ee38bcdb5f570bf1e9fc12
= <nguyenxuantoan2004@gmail.com>
2025-08-28 15:21:55 +0700

    Xây dựng UI phần Header #1

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,11 +1,32 @@
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 function Header() {
+    console.log(images.logo);
+
     return (
         <header className={clsx(styles.wrapper)}>
-            <div className={clsx(styles.inner)}></div>
+            <div className={clsx(styles.inner)}>
+                <div className={clsx(styles.logo)}>
+                    <img src={images.logo} alt="Tiktok logo" />
+                </div>
+                <div className={clsx(styles.search)}>
+                    <input placeholder="Search ..." />
+                    <button className={clsx(styles.clear)}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={clsx(styles.loading)} icon={faSpinner} />
+                    {/* loading... */}
+                    <button className={clsx(styles['search-btn'])}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={clsx(styles.action)}>action</div>
+            </div>
         </header>
     );
 }
