9291235b9e06881142f51ff5a3ecdbd5227915c8
= <nguyenxuantoan2004@gmail.com>
2025-09-01 22:22:15 +0700

    Giải quyết vấn đề nhiều file index.js | mới fix vài folder còn lại xem cách trước

---
+++ src/components/Button/Button.js
@@ -0,0 +1,68 @@
+import clsx from 'clsx';
+import { Link } from 'react-router';
+import styles from './Button.module.scss';
+
+// passProps dùng để ví dụ khi truyền target='_blank' thì sẽ nhận
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    disabled = false,
+    rounded = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    //Remove event listener when button disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && props[key] === typeof 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = clsx(styles.wrapper, {
+        [className]: className,
+        [styles.primary]: primary,
+        [styles.outline]: outline,
+        [styles.text]: text,
+        [styles.disabled]: disabled,
+        [styles.rounded]: rounded,
+        [styles.small]: small,
+        [styles.large]: large,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={clsx(styles.icon)}>{leftIcon}</span>}
+            <span className={clsx(styles.title)}>{children}</span>
+            {rightIcon && <span className={clsx(styles.icon)}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
---
+++ src/components/Popper/Menu/Menu.js
@@ -0,0 +1,69 @@
+import Tippy from '@tippyjs/react/headless';
+import clsx from 'clsx';
+
+import { Wrapper as PoperWrapper } from '~/components/Popper';
+import HeaderMenu from './HeaderMenu';
+import MenuItem from './MenuItem';
+import styles from './Menu.module.scss';
+import { useState } from 'react';
+import { data } from 'react-router';
+
+const defaultFn = () => {};
+
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+
+    const renderItems = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                            console.log('history', history);
+                            console.log('item', item);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            interactive
+            delay={[0, 300]}
+            offset={[12, 8]}
+            placement="top-end"
+            hideOnClick={hideOnClick}
+            render={(attrs) => (
+                <div className={clsx(styles['menu-list'])} tabIndex="-1" {...attrs}>
+                    <PoperWrapper className={clsx(styles['menu-poper'])}>
+                        {history.length > 1 && (
+                            <HeaderMenu
+                                title={'Languge'}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
+                        <div className={clsx(styles['menu-body'])}>{renderItems()}</div>
+                    </PoperWrapper>
+                </div>
+            )}
+            onHide={() => {
+                setHistory((prev) => prev.slice(0, 1));
+            }}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+export default Menu;
